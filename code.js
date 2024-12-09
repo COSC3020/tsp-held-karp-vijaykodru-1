337daa6ded36c20c2b5e77c669cadf37395431f7
Vijay Kodru <123325232+vijaykodru@users.noreply.github.com>
2024-12-09 15:02:45 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,31 +1,43 @@
 function tsp_hk(distance_matrix) {
-    const n = distance_matrix.length;
-    const cache = new Map();
-
-    function heldKarp(current, visited) {
-        // Base case: If all cities have been visited, return the distance to the start city (city 0)
-        if (visited === (1 << n) - 1) {
-            return 0
-        }
-
-        var key = JSON.stringify([visited, current]);
-
-        if (cache.has(key)) {
-            return cache.get(key);
-        }
-
-        let minDist = Infinity;
-
-        for (let nextCity = 0; nextCity < n; nextCity++) {
-            if ((visited & (1 << nextCity)) !== 0) continue;
-
-            const dist = distance_matrix[current][nextCity] + heldKarp(nextCity, visited | (1 << nextCity));
-            minDist = Math.min(minDist, dist);
-        }
-
-        cache.set(key, minDist);
-        return minDist;
+ 
+    // If there is only one or no city return 0
+    if (distance_matrix.length <= 1) {
+        return 0;
     }
 
-    return heldKarp(0, 1 << 0);
+    // dp will store the minimum distance to visit the cities in mask
+    //sets the ditance to be infinity
+    const dp = Array(1 << distance_matrix.length).fill().map(() => Array(distance_matrix.length).fill(Infinity));
+
+    //starting from each city and visiting only that city
+    for (let i = 0; i < distance_matrix.length; i++) {
+        dp[1 << i][i] = 0;
+    }
+
+    // Iterate over all possible subsets of cities (represented by 'mask')
+    for (let mask = 1; mask < (1 << distance_matrix.length); mask++) {
+        for (let last = 0; last < distance_matrix.length; last++) {
+            // Continue if last city is not in the current mask
+            if ((mask & (1 << last)) === 0) {
+                continue;
+            }
+
+            // Try to find the minimum distance by visiting a new city 'next'
+            for (let next = 0; next < distance_matrix.length; next++) {
+                if (next === last || (mask & (1 << next)) !== 0) {
+                    continue;
+                }
+
+                const newMask = mask | (1 << next);
+                dp[newMask][next] = Math.min(dp[newMask][next], dp[mask][last] + distance_matrix[last][next]);
+            }
+        }
+    }
+
+    // Find the minimum distance to visit all cities
+    let minTourDistance = Infinity;
+    for (let i = 0; i < distance_matrix.length; i++) {
+        minTourDistance = Math.min(minTourDistance, dp[(1 << distance_matrix.length) - 1][i]);
+    }
+    return minTourDistance;
 }
